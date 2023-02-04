e04622da84503fc6794b8c85288da729ffc93cac
DawidJasko <68279366+DawidJasko@users.noreply.github.com>
2023-02-04 04:20:53 +0100

    add dark mode

---
+++ script.js
@@ -0,0 +1,13 @@
+const toggelSwitch = document.querySelector('input[type="checkbox"]');
+
+// Switch Theme Dynamically
+function switchTheme(event) {
+  if (event.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+  }
+}
+
+// Event Listener
+toggelSwitch.addEventListener('change', switchTheme);
