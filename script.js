ae73df6caa3b23cc230ed6a3ad229f86a84987d3
DawidJasko <68279366+DawidJasko@users.noreply.github.com>
2023-02-06 02:22:38 +0100

    1 file modified

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const toggelSwitch = document.querySelector('input[type="checkbox"]');
 
+// Dark Mode Styles
+function darkMode() {
+  nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+  console.log(toggleIcon.children);
+}
+
 // Switch Theme Dynamically
 function switchTheme(event) {
   if (event.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
+    darkMode();
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
   }
