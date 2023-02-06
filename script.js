29b320f10cbf9808408891f9e7f6710db3b58195
DawidJasko <68279366+DawidJasko@users.noreply.github.com>
2023-02-06 03:13:37 +0100

    add moon icon

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 const toggelSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
 
 // Dark Mode Styles
 function darkMode() {
   nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
   textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-  console.log(toggleIcon.children);
+  toggleIcon.children[0].textContent = 'Dark Mode';
+  toggleIcon.children[1].classList.remove = 'fa-sun';
+  toggleIcon.children[1].classList.add = 'fa-moon';
+}
+
+// Light Mode Styles
+function lightMode() {
+  nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+  textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  toggleIcon.children[0].textContent = 'Light Mode';
+  toggleIcon.children[1].classList.remove = 'fa-moon';
+  toggleIcon.children[1].classList.add = 'fa-sun';
 }
 
 // Switch Theme Dynamically
@@ -14,6 +31,7 @@ function switchTheme(event) {
     darkMode();
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
+    lightMode();
   }
 }
 
